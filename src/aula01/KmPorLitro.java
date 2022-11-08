c21adc199099598c658d09101b12a6a36d87cf9f
andresoares37 <andresoares37@gmail.com>
2022-11-08 19:54:29 -0300

    ajustando as variáveis

--- src/aula01/KmPorLitro.java
+++ src/aula01/KmPorLitro.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class KmPorLitro {
 
 	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in);
-		float quilometroinicial;
-		float litros;
+		Scanner leitor = new Scanner(System.in);		
 		
 		System.out.println("Digite quantos quilometros do inicio da viagem:");
-		quilometroinicial = leitor.nextFloat();
+		float quilometroinicial = leitor.nextFloat();
 		System.out.println("Digite a quilometragem ao final da viagem:");
 		float quilometrofinal = leitor.nextFloat();
 		System.out.println("Digite quantos litros foram abastecidos:");
-		litros = leitor.nextFloat();
+		float litros = leitor.nextFloat();
+		
 		float quilometrosPorLitro = (quilometrofinal - quilometroinicial) / litros;
+		
 		leitor.close();
+		
 		System.out.print("A quantidade de quilometros por litros é de: " + quilometrosPorLitro);
 		
 
